064829e49c50658bb98e6dfb2e0459ce67054d5d
Griselda Cortez <griselda.cortez0218@gmail.com>
2021-04-10 23:04:14 -0400

    py

---
+++ average.py
@@ -0,0 +1,9 @@
+average = 0
+sum = 0
+for i in range(0, 4,): 
+ userinput = input("Number Please...")
+ usernum = input(10)
+sum = sum + usernum
+print("User number is; " + usernum + " add the current sum is; " + sum) 
+      
+  
---
+++ party.py
@@ -0,0 +1,9 @@
+userinput = input("let's party! how long until the party? (give me a number.)")
+usernum = input(10)
+if usernum <= 1:
+    print("PARTY NOW!")
+else:
+ for i in range(1, 10, 1):
+    print(i)
+    print("PARTY TIME!")
+
